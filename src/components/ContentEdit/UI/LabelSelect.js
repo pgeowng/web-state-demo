98d808a607dad935d7735e8fcfa6a6266e7a06e3
umgi <gtdsocial@icloud.com>
2021-03-26 07:49:53 +0300

    contentedit separation

--- src/components/ContentEdit/UI/LabelSelect.js
+++ src/components/ContentEdit/UI/LabelSelect.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 
+import { Select } from '../../UI'
+
 import { I18N } from '../i18n'
 import { Settings } from '../../../context/Settings'
 
@@ -10,20 +12,18 @@ export const LabelSelect = ({ id, optionEnum }) => {
 
   return (
     <div className="content-edit__line label-select">
-      <label htmlFor={id}>{i18n[id]}</label>
-      <select
+      <label htmlFor={id} className="text content-edit__left">
+        {i18n[id]}
+      </label>
+
+      <Select
+        className="content-edit__right"
         id={id}
         onChange={(e) => setSetting(id, e.target.value)}
         value={settings[id]}
       >
-        {i18n[optionEnum].map((text, idx) => {
-          return (
-            <option key={text} value={idx}>
-              {text}
-            </option>
-          )
-        })}
-      </select>
+        {i18n[optionEnum]}
+      </Select>
     </div>
   )
 }
